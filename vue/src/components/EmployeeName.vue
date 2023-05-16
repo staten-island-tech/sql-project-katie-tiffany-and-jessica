abf6f8e7cb348b94a40890c7bae4a5f32c500508
tiffyy101 <bobatiffy101@gmail.com>
2023-05-16 09:56:10 -0400

    2

---
+++ vue/src/components/EmployeeName.vue
@@ -0,0 +1,13 @@
+<template></template>
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ithhcayafesjeblmuhca.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data: Employees, error } = await supabase
+  .from('Employees')
+  .select('employee_name')
+
+</script>
