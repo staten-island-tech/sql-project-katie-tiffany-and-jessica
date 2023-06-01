bbd6ebeacd8ec1cf16766cce16156c39c89ebd36
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-06-01 11:02:35 -0400

    COMMIT WE DID IT!!! IT DELETED!!!

--- supabase-vue-3/src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createEmployee } from '@supabase/supabase-js'
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createEmployee(supabaseURL, supabaseAnonKey)
