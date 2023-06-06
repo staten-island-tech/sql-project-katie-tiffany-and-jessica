af81253af28263b3d7a9590864229aac946a811e
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-06-06 10:01:57 -0400

    ?????

--- supabase/src/views/AboutView.vue
+++ supabase/src/views/AboutView.vue
@@ -1,2 +1,8 @@
 <template>
+      <h1>Login</h1>
+  <h2>Email</h2>
+  <input type="text" id="user" />
+  <h2>Password</h2>
+  <input type="text" id="pw" />
+  <button>Enter</button>
 </template>
--- supabase/src/views/HomeView.vue
+++ supabase/src/views/HomeView.vue
@@ -4,10 +4,4 @@ import { ref, onMounted } from "vue";
 </script>
 
 <template>
-  <h1>Login</h1>
-  <h2>Email</h2>
-  <input type="text" id="user" />
-  <h2>Password</h2>
-  <input type="text" id="pw" />
-  <button>Enter</button>
 </template>
